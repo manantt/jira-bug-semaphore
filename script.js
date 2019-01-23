f6a738eb85e1ea6bafb5c0177cda959e0f92ccf9
Manuel Antón <manuel.anton@cosmomedia.es>
2019-01-23 11:27:57 +0100

    v0.2 FIX url matching. Added title to the bug links

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Jira bug semaphore
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
-// @author       You
-// @match        http://intranet.jira.es/browse/ACC-644?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20(%22Muy%20baja%22)%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC
+// @author       Manantt
+// @match        http://intranet.jira.es/*
 // @grant        none
 // ==/UserScript==
 
@@ -14,26 +14,31 @@
 var tickets = {
     "bloqueante" : {
         "cantidad" : 0,
+        "title":"Bloqueantes",
         "color" : "rgb(206, 0, 0)",
         "ruta" : 'http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20(Bloqueante)%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC'
     },
     "alta" : {
         "cantidad" : 0,
+        "title":"Prioridad alta",
         "color" : "rgb(234, 68, 68)",
         "ruta" : 'http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20(Alta)%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC'
     },
     "media" : {
         "cantidad" : 0,
+        "title":"Prioridad media",
         "color" : "rgb(234, 125, 36)",
         "ruta" : 'http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20(Media)%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC'
     },
     "baja" : {
         "cantidad" : 0,
+        "title":"Prioridad baja",
         "color" : "rgb(42, 135, 53)",
         "ruta" : 'http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20(Baja)%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC'
     },
     "muybaja" : {
         "cantidad" : 0,
+        "title":"Prioridad muy baja",
         "color" : "rgb(85, 165, 87)",
         "ruta" : 'http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20("Muy%20baja")%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC'
     },
@@ -59,11 +64,11 @@ function createMenu(numBloqueantes){
     var botones = '' +
         '<p class="aui-buttons" style="margin:0 50px">';
     $.each(tickets, function(key, value){
-        botones += '<button class="aui-button aui-button-primary" style="margin:0;background-color:'+value['color']+'"><a style="padding:0" href="'+value['ruta']+'" title="Ver bugs bloqueantes">'+(value['cantidad']>=50?"50+":value['cantidad'])+'</a></button>';
+        botones += '<button class="aui-button aui-button-primary" style="margin:0;background-color:'+value['color']+';width:42px;"><a style="padding:0" href="'+value['ruta']+'" title="'+value['title']+'">'+(value['cantidad']>=50?"50+":value['cantidad'])+'</a></button>';
     });
 
     botones += '<button class="aui-button aui-button-primary" style="margin:0"><a style="padding:0" title="Ver bugs sin asignar ordenados por prioridad" href=\'/issues/?jql=project%20%3D%20ACC%20AND%20statusCategory%20in%20("To%20Do"%2C%20"In%20Progress")%20AND%20(assignee%20%3D%20EMPTY%20OR%20assignee%20%3D%20currentUser())%20AND%20type%20%3D%20Error%20ORDER%20BY%20priority%20DESC%2C%20created%20ASC\'>Ver todos</a></button>' +
         '</p>';
 
-    $("#header nav .aui-header-primary ul").append(botones);
+    $("#header nav .aui-header-primary ul").first().append(botones);
 }
