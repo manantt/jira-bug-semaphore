c9254fd88398f3b73e6376d604e63ba96dfce395
Manuel Antón <manuel.anton@cosmomedia.es>
2019-01-25 08:58:37 +0100

    Corregido filtro de prioridad "Muy baja"

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jira bug semaphore
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Displays bugs count by colors according to priority
 // @author       Manantt
 // @match        http://intranet.jira.es/*
@@ -40,7 +40,7 @@ var tickets = {
         "cantidad" : 0,
         "title":"Prioridad muy baja",
         "color" : "rgb(85, 165, 87)",
-        "ruta" : 'http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20("Muy%20baja")%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC'
+        "ruta" : "http://intranet.jira.es/issues/?jql=project%20in%20(NET%2C%20NeT10-Bugs)%20AND%20priority%20in%20(\"Muy%20baja\")%20AND%20statusCategory%20in%20(%22To%20Do%22%2C%20%22In%20Progress%22)%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC"
     },
 };
 $.post( tickets['bloqueante']['ruta'], function( data ) {
@@ -63,7 +63,7 @@ $.post( tickets['bloqueante']['ruta'], function( data ) {
 function createMenu(numBloqueantes){
     var botones = '<p class="aui-buttons" style="margin:0 50px">';
     $.each(tickets, function(key, value){
-        botones += '<button class="aui-button aui-button-primary" style="margin:0;background-color:'+value['color']+';width:42px;"><a style="padding:0" href="'+value['ruta']+'" title="'+value['title']+'">'+(value['cantidad']>=50?"50+":value['cantidad'])+'</a></button>';
+        botones += '<button class="aui-button aui-button-primary" style="margin:0;background-color:'+value['color']+';width:42px;"><a style="padding:0" href='+'\''+value['ruta']+'\''+' title="'+value['title']+'">'+(value['cantidad']>=50?"50+":value['cantidad'])+'</a></button>';
     });
     botones += '<button class="aui-button aui-button-primary" style="margin:0"><a style="padding:0" title="Ver bugs sin asignar ordenados por prioridad" href=\'/issues/?jql=project%20%3D%20ACC%20AND%20statusCategory%20in%20("To%20Do"%2C%20"In%20Progress")%20AND%20(assignee%20%3D%20EMPTY%20OR%20assignee%20%3D%20currentUser())%20AND%20type%20%3D%20Error%20ORDER%20BY%20priority%20DESC%2C%20created%20ASC\'>Ver todos</a></button>';
     botones += '</p>';
