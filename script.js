bc022120f74e4b89a4e49efbbcbe1d77cddfda77
Manuel Antón <manuel.anton@cosmomedia.es>
2019-08-27 08:59:46 +0200

    desactivado parpadeo de alerta

--- script.js
+++ script.js
@@ -290,7 +290,7 @@ function crearDashboard(){
     css += '.contenedor-dashboard {width: '+ancho+'px;height: 28px;position: absolute;top: 0;left: 32px;background:#f5f5f5;border:1px solid #ccc;border-left: none;border-top:none;border-right:none}';
     css += '.contenedor {width:20px;height:28px;display:inline-block; margin:4px 6px}';
     css += '#opciones {width:15px;height:15px;display:inline-block; margin:4px 6px; float:right;cursor:pointer}';
-    css += '.alertando{animation: alerta .5s infinite}';
+    css += '.alertando{animation: alerta .5s forwards}';
     css += '@keyframes alerta {from {fill: #555;}to {fill: #dc3545;}}';
 
     css += '/* The Modal (background) */.modal {  display: none; /* Hidden by default */  position: fixed; /* Stay in place */  z-index: 1; /* Sit on top */  left: 0;  top: 0;  width: 100%; /* Full width */  height: 100%; /* Full height */  overflow: auto; /* Enable scroll if needed */  background-color: rgb(0,0,0); /* Fallback color */  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */}/* Modal Content/Box */.modal-content {  background-color: #fefefe;  margin: 15% auto; /* 15% from the top and centered */  padding: 20px;  border: 1px solid #888;  width: 80%; /* Could be more or less, depending on screen size */}/* The Close Button */.close {  color: #aaa;  float: right;  font-size: 28px;  font-weight: bold;}.close:hover,.close:focus {color: black;text-decoration: none;cursor: pointer;}';
