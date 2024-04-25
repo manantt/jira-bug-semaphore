3ab838ba6ccb5859a53d37de8b9fc84f411b79a9
manantt <manantt@hotmail.com>
2024-04-25 11:09:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jira bug semaphore
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Displays bugs count by colors according to priority
 // @author       Manantt
 // @match        http://intranet.jira.es/*
